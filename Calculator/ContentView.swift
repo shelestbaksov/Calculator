5cf8eec309c3f06500514e02967ce0ceedb8a62d
Leisan Latypova <latley90@gmail.com>
2022-08-19 16:53:36 +0300

    Added calculating logic

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 enum CalculatorButton: String {
     case zero, one, two, three, four, five, six, seven, eight, nine
-    case equals, plus, minus, multiple, divide
-    case ac, plusMinus, percent, dot
+    case equals, plus, minus, multiply, divide
+    case ac, plusMinus, percent, decimal
     
     var title: String {
         switch self {
@@ -27,18 +27,18 @@ enum CalculatorButton: String {
         case .equals: return "="
         case .plus: return "+"
         case .minus: return "-"
-        case .multiple: return "X"
+        case .multiply: return "X"
         case .divide: return "/"
         case .ac: return "AC"
         case .plusMinus: return "+/-"
         case .percent: return "%"
-        case .dot: return "."
+        case .decimal: return "."
         }
     }
     
     var backgroundColor: Color {
         switch self {
-        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .dot:
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .decimal:
             return Color(.darkGray)
         case .ac, .plusMinus, .percent:
             return Color(.lightGray)
@@ -48,24 +48,22 @@ enum CalculatorButton: String {
     }
 }
 
-class GlobalEnvironment: ObservableObject {
-    @Published var display = ""
-    
-    func receiveInput(calculatorButton: CalculatorButton) {
-        self.display = calculatorButton.title
-    }
+enum Operation {
+    case plus, minus, multiply, divide, none
 }
 
 struct ContentView: View {
     
-    @EnvironmentObject var environment: GlobalEnvironment
+    @State var value = "0"
+    @State var runningNumber = 0
+    @State var currentOperation: Operation = .none
     
     let buttons: [[CalculatorButton]] = [
         [.ac, .plusMinus, .percent, .divide],
-        [.seven, .eight, .nine, .multiple],
+        [.seven, .eight, .nine, .multiply],
         [.four, .five, .six, .minus],
         [.one, .two, .three, .plus],
-        [.zero, .dot, .equals]
+        [.zero, .decimal, .equals]
     ]
     
     var body: some View {
@@ -74,49 +72,85 @@ struct ContentView: View {
             VStack (spacing: 12) {
                 HStack{
                     Spacer()
-                    Text(environment.display).foregroundColor(.white)
+                    Text(value).foregroundColor(.white)
                         .font(.system(size: 64))
                 }.padding()
                 ForEach(buttons, id: \.self) { row in
                     HStack (spacing: 12) {
                         ForEach(row, id: \.self) { button in
-                           CalculatorButtonView(button: button)
+                            Button {
+                                self.didTap(button: button)
+                            } label: {
+                                Text(button.title)
+                                    .font(.system(size: 32))
+                                    .frame(width: self.buttonWidth(button: button), height: (UIScreen.main.bounds.width - 5 * 12) / 4)
+                                    .foregroundColor(.white)
+                                    .background(button.backgroundColor)
+                                    .cornerRadius(self.buttonWidth(button: button))
+                            }
                         }
                     }
                 }
             }.padding(.bottom)
         }
     }
-
-}
-
-struct CalculatorButtonView: View {
     
-    var button: CalculatorButton
-    @EnvironmentObject var environment: GlobalEnvironment
     
-    var body: some View {
-        Button {
-            self.environment.receiveInput(calculatorButton: self.button)
-        } label: {
-            Text(button.title)
-                .font(.system(size: 32))
-                .frame(width: self.buttonWidth(button: button), height: (UIScreen.main.bounds.width - 5 * 12) / 4)
-                .foregroundColor(.white)
-                .background(button.backgroundColor)
-                .cornerRadius(self.buttonWidth(button: button))
-        }
-    }
-    private func buttonWidth(button: CalculatorButton) -> CGFloat {
+    
+    func buttonWidth(button: CalculatorButton) -> CGFloat {
         if button == .zero {
             return (UIScreen.main.bounds.width - 4 * 12) / 4 * 2
         }
         return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
+    
+    func didTap(button: CalculatorButton) {
+        switch button {
+        case .plus, .minus, .multiply, .divide, .equals :
+            if button == .plus {
+                self.currentOperation = .plus
+                self.runningNumber += Int(self.value) ?? 0
+            } else if button == .minus {
+                self.currentOperation = .minus
+                self.runningNumber += Int(self.value) ?? 0
+            } else if button == .multiply {
+                self.currentOperation = .multiply
+                self.runningNumber += Int(self.value) ?? 0
+            } else if button == .divide {
+                self.currentOperation = .divide
+                self.runningNumber += Int(self.value) ?? 0
+            } else if button == .equals {
+                let runningValue = self.runningNumber
+                let currentValue = Int(self.value) ?? 0
+                switch self.currentOperation{
+                case .plus: self.value = "\(runningValue + currentValue)"
+                case .minus: self.value = "\(runningValue - currentValue)"
+                case .divide: self.value = "\(runningValue / currentValue)"
+                case .multiply: self.value = "\(runningValue * currentValue)"
+                case .none:  break
+                    
+                }
+            }
+            if button != .equals {
+                self.value = "0"
+            }
+        case .ac:
+            value = "0"
+        case .plusMinus, .percent, .decimal:
+            break
+        default:
+            let number = button.title
+            if self.value == "0" {
+                value = number
+            } else {
+                self.value = "\(self.value)\(number)"
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environmentObject(GlobalEnvironment())
+        ContentView()
     }
 }
